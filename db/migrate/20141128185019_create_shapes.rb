f4ebd88e604cb3e723bc1fb4a98320f2521c90d1
Chloe Reimer <chloe.reimer@gmail.com>
2014-11-30 12:41:09 -0500

    fixes migration file for shapes

--- db/migrate/20141128185019_create_shapes.rb
+++ db/migrate/20141128185019_create_shapes.rb
@@ -1,13 +1,13 @@
 class CreateShapes < ActiveRecord::Migration
 
   def up
-    create_table :shapes do |t|
+    create_table :shapes, { id: false } do |t|
+      t.string :shape_id
       t.string :pt_lat
       t.string :pt_lon
       t.string :pt_sequence
       t.string :dist_traveled
     end
-    change_column :shapes, :id, :string
   end
 
   def down
